40b81864ca02d5051d6aa6f5b1cf7aa52197ea74
Andrei Ilin <stariktenger@gmail.com>
2025-10-05 14:27:11 +0200

    block levels

--- scripts/UI/LevelManager.gd
+++ scripts/UI/LevelManager.gd
@@ -3,6 +3,7 @@ extends Control
 @export var level_button_scene: PackedScene
 @onready var grid: GridContainer = $GridContainer
 var current_level: int = 0
+var max_opened_level: int = 0
 
 var level_scenes: Array[String] = [
 	"res://levels/level_template.tscn",
@@ -11,6 +12,13 @@ var level_scenes: Array[String] = [
 ]
 
 var levels_by_id: Dictionary = {}
+
+# Update the state of level buttons based on the current progress
+func _update_level_buttons():
+	for i in range(level_scenes.size()):
+		var btn = grid.get_child(i)
+		if btn:
+			btn.is_opened = i <= max_opened_level
 
 func _ready():
 	print("Start")
@@ -36,6 +44,9 @@ func populate_levels():
 		btn.level_selected.connect(_on_level_selected)
 		grid.add_child(btn)
 	grid.get_child(0).grab_focus()
+
+	_update_level_buttons()
+
 	print("Populated ", level_scenes.size(), " level buttons")
 
 func _launch_level(level_id: int):
@@ -53,6 +64,7 @@ func _on_level_selected(level_id: int):
 func next_level():
 	if current_level + 1 < level_scenes.size():
 		current_level += 1
+		max_opened_level = max(max_opened_level, current_level)
 		print("Loading next level ", current_level)
 		_launch_level(current_level)
 	else:
