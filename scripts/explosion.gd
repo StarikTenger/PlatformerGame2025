cbf33f248a23cb064c5d67f2f9f9021ece904f66
Andrei Ilin <stariktenger@gmail.com>
2025-10-06 02:39:29 +0200

    explosion

--- scripts/explosion.gd
+++ scripts/explosion.gd
@@ -1,21 +1,9 @@
 extends Node2D
 
-@export var grow_time: float = 0.2
-@export var max_scale: float = 2.0
-
-var _timer := 0.0
-var _original_scale := Vector2.ONE
-
-
 func _ready():
-	_original_scale = scale
 	#scale = Vector2.ZERO
 	print("Explosion at position: ", global_position)
+	rotation = randf() * TAU
 
-func _process(delta):
-	_timer += delta
-	if _timer < grow_time:
-		var t = _timer / grow_time
-		#scale = _original_scale.lerp(_original_scale * max_scale, t)
-	else:
-		queue_free()
+	$AnimatedSprite2D.connect("animation_finished", Callable(self, "queue_free"))
+	$AnimatedSprite2D.play()
